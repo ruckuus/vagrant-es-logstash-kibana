b8a3540c1d5a0ba041e0c3c4b95df5f73c6abfce
Dwi Sasongko S <ruckuus@gmail.com>
2014-07-16 23:54:11 +0800

    Added logstash configuration.

--- puppet/manifests/node/front_end.pp
+++ puppet/manifests/node/front_end.pp
@@ -14,3 +14,7 @@ class { 'logstash':
    package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.1-1-bd507eb_all.deb',
    require => Package['openjdk-7-jre-headless']
 }
+
+logstash::configfile { 'input_nginx':
+  content => template('logstash/logstash_input_nginx.erb')
+}
